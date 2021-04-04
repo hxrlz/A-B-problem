003205fbf87f72aa092bfa4188ad3a70fbf1f5a0
hxrlz <81855262+hxrlz@users.noreply.github.com>
2021-04-04 11:29:43 +0800

    Add files via upload

---
+++ A+B problem.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ 
+
+public class Main 
+{
+ 
+	
+public static void main(String[] args) 
+{
+		
+// TODO Auto-generated method stub
+		
+Scanner in= new Scanner(System.in);
+		
+int a=123,b=456;
+		
+a=in.nextInt();
+		
+b=in.nextInt();
+		
+System.out.println(a+b);
+	}
+ 
+}
+
